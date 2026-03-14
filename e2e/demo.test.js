b5404730cbfe377a654fc738ad0f26761dea223b
michaelh440 <michael.hyland@yahoo.com>
2026-03-13 19:30:15 -0500

    Added changes to allow multiple apps to run on a single domain

--- e2e/demo.test.js
+++ e2e/demo.test.js
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
+	await page.goto('/gft');
 	await expect(page.locator('h1')).toBeVisible();
 });
