d993d96f23d99c42f2b416cda50e91a024eb0c2c
michaelh440 <michael.hyland@yahoo.com>
2026-03-14 11:42:23 -0500

    updated env file to connect to NEON db as well as local

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,10 +2,13 @@
 import postgres from 'postgres';
 import { DATABASE_URL } from '$env/static/private';
 
+const isNeon = DATABASE_URL.includes('neon.tech');
+
 const sql = postgres(DATABASE_URL, {
 	max: 10,
 	idle_timeout: 20,
-	connect_timeout: 10
+	connect_timeout: 10,
+	ssl: isNeon ? 'require' : false
 });
 
 export default sql;
